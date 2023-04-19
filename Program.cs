674c0d66b3beb14ab7c1a4667652d73c8ddaa6a4
Non <warud.seso@mail.kmutt.ac.th>
2023-04-20 03:08:05 +0700

    Market

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Quiz3 {
+class Program {
+
+// Market
+    static void Main(string []args){
+
+        int total = int.Parse(Console.ReadLine());
+
+        
+
+    }
+
+
+
+
+
+
+}
+
+}
